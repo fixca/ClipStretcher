64216876ff5cab240a10ef7ccdc1418a2745835d
fixca <jaehoon1744@gmail.com>
2023-10-25 22:48:32 +0900

    커밋

--- src/main/java/me/fixca/clipstretcher/executor/ExecutorHandler.java
+++ src/main/java/me/fixca/clipstretcher/executor/ExecutorHandler.java
@@ -11,14 +11,33 @@ import me.fixca.clipstretcher.controller.MainController;
 import me.fixca.clipstretcher.storage.ClipRepository;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 public class ExecutorHandler {
 
     @Getter
     @Setter
     private static boolean executing = false;
+
     private static int originalAmount = -1;
     private static DoubleProperty property;
+
+    private static void preRendering(Clip clip) {
+        property.add(1 / originalAmount);
+        ClipRepository.removeSelectedClip(clip);
+        if (ClipRepository.getAllSelectedClips().size() == 0) {
+            executing = false;
+            LineCollector.reset();
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("완료");
+                alert.showAndWait();
+                MainController.getInstance().updateView();
+                MainController.getInstance().getProgressBar().setProgress(0);
+                MainController.getInstance().enableButtons();
+            });
+        }
+    }
 
     public static void start() {
         if (executing) {
@@ -33,26 +52,20 @@ public class ExecutorHandler {
             MainController.getInstance().getProgressBar().progressProperty().bind(property);
         });
 
-        for (Clip clip : ClipRepository.getAllSelectedClips()) {
-            CompletableFuture<Integer> completableFuture = CommandExecutor.executeStretch(clip);
-            completableFuture.thenRunAsync(() -> {
-                property.add(1 / originalAmount);
-                ClipRepository.removeSelectedClip(clip);
-                if (ClipRepository.getAllSelectedClips().size() == 0) {
-                    executing = false;
-                    LineCollector.reset();
-                    Platform.runLater(() -> {
-                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                        alert.setContentText("완료");
-                        alert.showAndWait();
-                        MainController.getInstance().updateView();
-                        MainController.getInstance().getProgressBar().setProgress(0);
-                        MainController.getInstance().enableButtons();
+        Executors.newSingleThreadExecutor().execute(() -> {
+            for (Clip clip : ClipRepository.getAllSelectedClips()) {
+                if(MainController.getInstance().getCheckMultiThreadItem().isSelected()) {
+                    CompletableFuture<Integer> completableFuture = CommandExecutor.executeStretchAsync(clip);
+                    completableFuture.thenRunAsync(() -> {
+                        preRendering(clip);
                     });
                 }
-            });
-        }
-
+                else {
+                    CommandExecutor.executeStretch(clip);
+                    preRendering(clip);
+                }
+            }
+        });
     }
 
 }
